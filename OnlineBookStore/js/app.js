02faac7a58c780d9b23416760a733c10392c92e9
Jordan Wren <jordanwren@Jordans-MacBook-Pro.local>
2017-04-09 22:51:38 -0400

    Added Functionality

--- OnlineBookStore/js/app.js
+++ OnlineBookStore/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation();
+(function ( $ ) {
+  $.fn.progress = function() {
+    var percent = this.data("percent");
+    this.css("width", percent+"%");
+  };
+}( jQuery ));
+
+$(document).ready(function(){
+  $(".bar-one .bar").progress();
+  $(".bar-two .bar").progress();
+  $(".bar-three .bar").progress();
+});
